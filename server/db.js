71539bbad0c65761160201e8f7b23da411e87df4
SameelaRathnagari <rathnagarisameela@gmail.com>
2026-04-27 00:48:26 +0530

    fix: use database_url

--- server/db.js
+++ server/db.js
@@ -1,17 +1,10 @@
 const mysql = require("mysql2");
-require("dotenv").config(); // ✅ load env variables
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306 // ✅ optional but safe
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect(err => {
     if (err) {
-        console.log("DB Connection Error:", err);
+        console.log("DB Error:", err);
     } else {
         console.log("DB Connected");
     }
